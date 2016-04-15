8547a13496aedce0aad51b45cf8b4c015cebd905
Benjamin Gnewuch <bgnewuch@yahoo.com>
2016-04-15 16:12:31 -0700

    Created a test app.spec.js file to test the routes

---
+++ app.spec.js
@@ -0,0 +1,42 @@
+var chai = require('chai');
+var request = require('request');
+var assert = chai.assert;
+
+var app = require('./app.js');
+var RANDOMIZE = 0;
+var server = app.listen(RANDOMIZE);
+var port = server.address().port;
+
+describe('Todos can', function() {
+
+  it('be returned.', function(done) {
+    request('http://localhost:' + port + 'todos/Ben', function(error, response, body) {
+      assert.equal(response.statusCode, 200);
+      done();
+    });
+  });
+
+  it('be added.', function(done) {
+    request({
+      url: 'http://localhost:' + port + 'todo',
+      method: 'POST',
+      headers: {'Content-type': 'application/json',
+      body: JSON.stringify({task: 'Test'})
+    },
+    function(error, response, body) {
+      assert.equal(response.statusCode, 200);
+      done();
+    });
+  });
+
+  it('can be finished.', function(done) {
+    request({
+      url: 'http://localhost:' + port + '/todoFinish/Test',
+      method: 'DELETE'
+    },
+    function(error, response, body) {
+      assert.equal(response.statusCode, 200);
+      done();
+    });
+  });
+});
