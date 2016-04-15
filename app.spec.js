f753eacf8bba57e2777db47fde17e141d05fc527
Benjamin Gnewuch <bgnewuch@yahoo.com>
2016-04-15 16:28:42 -0700

    Completed the test file, and all tests are passing

--- app.spec.js
+++ app.spec.js
@@ -7,20 +7,19 @@ var RANDOMIZE = 0;
 var server = app.listen(RANDOMIZE);
 var port = server.address().port;
 
-describe('Todos can', function() {
-
-  it('be returned.', function(done) {
-    request('http://localhost:' + port + 'todos/Ben', function(error, response, body) {
+describe('Todos', function() {
+  it('can be returned.', function(done) {
+    request('http://localhost:' + port + '/todos/Ben', function(error, response, body) {
       assert.equal(response.statusCode, 200);
       done();
     });
   });
 
-  it('be added.', function(done) {
+  it('can be added.', function(done) {
     request({
-      url: 'http://localhost:' + port + 'todo',
+      url: 'http://localhost:' + port + '/todo',
       method: 'POST',
-      headers: {'Content-type': 'application/json',
+      headers: {'Content-type': 'application/json'},
       body: JSON.stringify({task: 'Test'})
     },
     function(error, response, body) {
