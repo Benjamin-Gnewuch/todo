78165dd6105b64f9fa3cbca5fff22018e7fb1b8f
Benjamin Gnewuch <bgnewuch@yahoo.com>
2016-04-18 16:13:31 -0700

    Set up todo to use a route provider and view, so sending requests can simply pull html files from the front end without consulting the backend

--- public/app.js
+++ public/app.js
@@ -1,1 +1,15 @@
-var app = angular.module('todo', []);
+var app = angular.module('todo', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+  .when("/todo", {
+    templateUrl: "/todo/viewtodo.html",
+    controller: "todoController",
+    controllerAs: "todo"
+  })
+  .when("/home", {
+    templateUrl: "/home/viewhome.html",
+    controller: "homeController",
+    controllerAs: "home"
+  })
+}]);
