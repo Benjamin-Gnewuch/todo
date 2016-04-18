132f4e46b14ded8f3ee75066b3b83c59937c1fdd
Benjamin Gnewuch <bgnewuch@yahoo.com>
2016-04-18 10:20:17 -0700

    Added a gulpfile to run my test doc automatically.

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
+var mocha = require('gulp-mocha');
+
+gulp.task('default', function() {
+  nodemon({ script: 'app.js' })
+  .on('start', ['test']);
+});
+
+gulp.task('test', function() {
+  return gulp.src('app.spec.js').pipe(mocha());
+})
+
+gulp.task('dev', function() {
+  return gulp.src('app.spec.js').pipe(mocha());
+})
