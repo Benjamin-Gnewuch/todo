57199c597e345a8318a4d1321eb750401dff9015
Benjamin Gnewuch <bgnewuch@yahoo.com>
2016-04-22 10:56:51 -0700

    Moved failing test to the end to see if it causes the new first test to fail, and allows it to pass

--- casper-test.js
+++ casper-test.js
@@ -2,16 +2,6 @@ casper.test.begin('Todo test', 3, function suite(test) {
   casper.start('http://localhost:1337' , function() {
 
     casper.then(function() {
-      this.click('#home-button');
-      casper.waitForSelector('h2', function() {
-        casper.wait(2000, function() {});
-        if(this.exists('h2.col-md-12')) {
-          test.assertSelectorHasText('h2.col-md-12', 'Welcome Home Ben!');
-        }
-      });
-    });
-
-    casper.then(function() {
       this.click('#todo-button');
       casper.waitForSelector('#add-text', function() {
         casper.sendKeys('#add-text', 'Test');
@@ -32,6 +22,16 @@ casper.test.begin('Todo test', 3, function suite(test) {
       casper.wait(500, function() {
         test.assertSelectorDoesntHaveText('ul > li:last-child > span.ng-binding', 'Test');
       })
+    });
+
+    casper.then(function() {
+      this.click('#home-button');
+      casper.waitForSelector('h2', function() {
+        casper.wait(2000, function() {});
+        if(this.exists('h2.col-md-12')) {
+          test.assertSelectorHasText('h2.col-md-12', 'Welcome Home Ben!');
+        }
+      });
     });
   });
 
